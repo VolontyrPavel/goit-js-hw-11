fd62e5d8320c4bb5191be8eead5f2d0f4f77922e
VolontyrPavel <pavel.volontyr@gmail.com>
2023-05-27 19:21:50 +0300

    2

--- src/index.js
+++ src/index.js
@@ -2,35 +2,50 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 const lightbox = new SimpleLightbox('.gallery a');
 
-const newGallery = document.querySelector('.gallery');
-
 import Notiflix from 'notiflix';
 
+let counterPage = 1;
+
 const form = document.querySelector('#search-form');
-
 form.addEventListener('submit', onSubmit);
+const newGallery = document.querySelector('.gallery');
+const guard = document.querySelector('.js-guard');
 
-async function onSubmit(e) {
+const refs = {
+    url: 'https://pixabay.com/api/',
+    key: '36730678-336aeda16ae09d290d6765b0a',
+    q: `${find}`,
+    page: counterPage,
+}
+
+let options = {
+    root: null,
+    rootMargin: '400px',
+    threshold: 0,
+}
+let observer = new IntersectionObserver(scroll, options);
+
+function onSubmit(e) {
     e.preventDefault();
     newGallery.innerHTML = '';
+    refs.page = 1;
+
     const findFor = form.elements.searchQuery.value;
 
-    try {
-        const data = await findImg (findFor);
-        makeMarkup(data);
-    } catch (error) {
-        console.log(error.message);
-    }
+    findImg (findFor)
+    .then (makeMarkup)
+    .catch ((err) => {
+        if (err.message === '404') {
+            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        }
+        console.log(err);
+    });
 }
 
 function findImg(find) {
-    const refs = {
-        url: 'https://pixabay.com/api/',
-        key: '36730678-336aeda16ae09d290d6765b0a',
-        q: `${find}`,
-    }
+    refs.q = `${find}`;
 
-    return fetch(`${refs.url}?key=${refs.key}&q=${refs.q}}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`)
+    return fetch(`${refs.url}?key=${refs.key}&q=${refs.q}}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${refs.page}`)
     .then((response) => {
         if(!response.ok) {
             throw new Error(response.status);
@@ -45,9 +60,9 @@ function makeMarkup(data) {
     if (data.hits.length !== 0) {
         Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
         return newGallery.innerHTML = data.hits.map(({ largeImageURL, previewURL, tags, likes, views, comments, downloads }) => 
-        `<div class="photo-card">
-            <a href='${largeImageURL}'>
-                <img src="${previewURL}" alt="${tags}" loading="lazy" />
+        `<div class="photo-card list">
+            <a clas='gallery__link' href='${largeImageURL}'>
+                <img class='gallery__image' src="${previewURL}" alt="${tags}" loading="lazy" />
             </a>
             <div class="info">
                 <p class="info-item">
@@ -71,6 +86,4 @@ function makeMarkup(data) {
     }
     return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
 }
-
-
 
